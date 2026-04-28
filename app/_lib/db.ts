4652799c510a4b748eedb6b52761435596409650
Hanzlah Ch <hanzlahch1234@gmail.com>
2026-04-29 01:10:38 +0500

    add MongoDB connection utility

---
+++ app/_lib/db.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+type MongooseCached = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoose: MongooseCached | undefined;
+}
+
+const cached = global._mongoose || (global._mongoose = { conn: null, promise: null });
+
+async function connect() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connect;
